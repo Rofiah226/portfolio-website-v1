38854a87168c605a31fb879cd43061c42dda3801
rofiah <akintundeadenike020606@gmail.com>
2026-04-06 13:47:30 +0100

    was able to fix somethings

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
-const toggler=document.getElementById('moon')
-// const body1=document.getElementsById('body')
+const toggle=document.getElementById('moon')
+// const body=document.getElementById('body')
 // const nav=document.getElementById('nav-bar')
 // const container=document.getElementById('container')
 
 
 
 
-toggler.addEventListener('click',()=>{
-    toggler.style.color='white'
+toggle.addEventListener('click',()=>{
+    // toggle.style.color='white'
     // document.body.style.backgroundColor='#2c4e35'
     // nav.style.backgroundColor='#2c4e35'
     // container.style.backgroundColor='red'
     // document.body.style.backgroundColor='#2c4e35'
 
+    // body.style.backgroundColor='#2c4e35'
 
-if(toggler.checked=== true){
-       document.body.style.backgroundColor='black'
-    }else{
-         document.body.style.backgroundColor='white'
+    document.body.classList.toggle('dark-mode')
 
-    }
+
 
 
 
